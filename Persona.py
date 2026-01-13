2f91c0527e11c0875368294112db29981c238121
andreabrcns <u3761506554@gmail.com>
2026-01-13 19:40:18 +0100

    atributos persona

---
+++ Persona.py
@@ -0,0 +1,6 @@
+class Persona:
+    def __init__(self, nombre, raza, edad, genero):
+            self.__nombre = nombre
+            self.__raza = raza
+            self.__edad = edad
+            self.__genero = genero
