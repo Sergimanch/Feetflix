b08c990b998fff18dc3a642e534cc80f23ecc02e
Sergimanch <lopezlianessergio@gmail.com>
2026-02-03 20:18:46 +0100

    Vista finalizada con menus para admin y contenido y más metodos para imprimir

--- controlador/Controlador.py
+++ controlador/Controlador.py
@@ -1,2 +1,18 @@
-import modelo
+from modelo.Contenido import Contenido
+from modelo.Usuario import Usuario
+from modelo.Admin import Admin
+from vista.Vista import Vista
 
+class Controlador:
+    """
+    Controlador principal. Coordina Vista ↔ Modelo.
+    Mantiene listas de usuarios, admins y catálogo.
+    """
+
+    def __init__(self):
+        self.__catalogo: list[Contenido] = []
+        self.__usuarios: list[Usuario] = []
+        self.__admins: list[Admin] = []
+        self.__vista = Vista()
+        self.__usuario_actual = None
+        self.__admin_actual = None
